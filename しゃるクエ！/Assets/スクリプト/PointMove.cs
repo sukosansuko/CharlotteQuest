fbf50a8be09961a4356142bd4f54aa8a655cba9c
a <a@aa.a.a>
2019-06-18 15:59:50 +0900

    UI、アニメーションの編集

---
+++ しゃるクエ！/Assets/スクリプト/PointMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointMove : MonoBehaviour
+{
+    int moveCnt;
+    Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveCnt = 0;
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveCnt++;
+        if(moveCnt / 15 % 2 == 0)
+        {
+            image.transform.position += new Vector3(0, 0.01f, 0);
+        }
+        else
+        {
+            image.transform.position += new Vector3(0, -0.01f, 0);
+        }
+    }
+}
